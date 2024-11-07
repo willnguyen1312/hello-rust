ccc5ddaa434e224bd02190be4eaf10e781a70106
Nam Nguyen <nam.nguyen@shopify.com>
2024-11-07 10:56:58 -0700

    Enum ✨

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,26 @@
-fn main() {
-    let my_string = String::from("hello world");
-
-    // `first_word` works on slices of `String`s, whether partial or whole
-    let word = first_word(&my_string[0..6]);
-    let word = first_word(&my_string[..]);
-    // `first_word` also works on references to `String`s, which are equivalent
-    // to whole slices of `String`s
-    let word = first_word(&my_string);
-
-    let my_string_literal = "hello world";
-
-    // `first_word` works on slices of string literals, whether partial or whole
-    let word = first_word(&my_string_literal[0..6]);
-    let word = first_word(&my_string_literal[..]);
-
-    // Because string literals *are* string slices already,
-    // this works too, without the slice syntax!
-    let word = first_word(my_string_literal);
-
-    println!("{}", word);
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
 
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
+fn main() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
         }
     }
 
-    &s[..]
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
