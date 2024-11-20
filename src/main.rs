7d3b5e8dcbaffbea12a7528a73e1ba1275646a81
Nam Nguyen <nam.nguyen@shopify.com>
2024-11-19 18:59:00 -0700

    Let's go Rust ✨

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io;
+
+fn main() -> io::Result<()> {
+    // Get the current working directory
+    let current_dir = env::current_dir()?;
+
+    // Print the current working directory
+    println!("Current working directory: {}", current_dir.display());
+
+    Ok(())
 }
