7c157239a94c519cb3ecadb5732cbaf6ea0b318c
Nam Nguyen <nam.nguyen@shopify.com>
2024-12-06 09:18:34 -0700

    lol

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const fs = require("fs");
 fs.readFile("./sample.txt", "utf8", (_, data) => {
   console.log(data);
 });
+
+// last_commit_message=$(git log -1 --pretty=%B) && git reset HEAD~1 && git add . && git commit -m "$last_commit_message" && git push -f
